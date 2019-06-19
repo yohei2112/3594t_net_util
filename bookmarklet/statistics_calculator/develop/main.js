e1113739d5e85142cb405903f027122607067c4d
yohei2112 <yohei2112@gmail.com>
2019-06-20 02:38:48 +0900

    総合ページで使用回数TOP10に入っていない武将の情報も計算するようにした

--- bookmarklet/statistics_calculator/develop/main.js
+++ bookmarklet/statistics_calculator/develop/main.js
@@ -7,9 +7,19 @@ function main() {
   }
 
   const frameList = document.getElementsByClassName("frame01");
-  const generalUseCountList = getGeneralUseCountList(frameList[0]);
-  for(var i=1; i<frameList.length; i++) {
-    calcCountPerUse(generalUseCountList, frameList[i]);
+  var generalUseCountList = getGeneralUseCountList(frameList[0]);
+
+  if(location.search == "?type=0") {
+    getGeneralUseCountFromApi().then((result) => {
+      generalUseCountList = Object.assign(result, generalUseCountList)
+      for(var i=1; i<frameList.length; i++) {
+        calcCountPerUse(generalUseCountList, frameList[i]);
+      }
+    });
+  } else {
+    for(var i=1; i<frameList.length; i++) {
+      calcCountPerUse(generalUseCountList, frameList[i]);
+    }
   }
 }
 
@@ -34,6 +44,7 @@ function updateCountText(frameTitle, countElement, generalUseCount) {
   const calclatedCount = frameTitle == "勝利数" ? (100 * orgCount / generalUseCount).toFixed(3) : (orgCount / generalUseCount).toFixed(3);
   countElement.style.lineHeight = "32px";
   countElement.style.paddingTop = "4px";
+  countElement.style.whiteSpace = "nowrap";
   const unit = frameTitle == "勝利数" ? "%" : "回/戦";
   countElement.innerHTML = countElement.innerHTML + "<br>" + calclatedCount + "<span class='font_80 ml_5'>" + unit + "</span>";
 }
@@ -58,4 +69,65 @@ function getAvatarHash(listElement) {
 
 function getCount(countElement) {
   return parseInt(countElement.textContent.split("回")[0]);
+}
+
+function getGeneralUseCountFromApi() {
+  return Promise.all([getGeneralIndexAndUseCountListFromApi(), getGeneralBaseDataFromApi()]).then((values) => {
+    const generalIndexAndUseCountList = values[0];
+    const generalBaseDataList = values[1];
+    const top10list = document.getElementsByClassName("top10list");
+    var generalUseCountList = [];
+    var avatarHash = "";
+    for(var i=0; i<top10list.length; i++) {
+      avatarHash = getAvatarHash(top10list[i]);
+      generalUseCountList[avatarHash] = getGeneralUseCountByAvatarHash(generalIndexAndUseCountList, generalBaseDataList, avatarHash);
+    }
+    return generalUseCountList;
+  })
+}
+
+function getGeneralIndexAndUseCountListFromApi() {
+  return new Promise((resolve) => {
+    const DATA_LIST_API_URL = "https://3594t.net/datalist/api/members/main";
+    const GENERAL_USE_COUNT_INDEX = 3;
+    const request = new XMLHttpRequest();
+    request.open("GET", DATA_LIST_API_URL);
+    request.addEventListener("load", (event) => {
+      if (event.target.status !== 200) {
+        alert("データ取得に失敗しました、ステータスコード：" + event.target.status);
+        return;
+      }
+      const bodyArray = event.target.responseText.split("\n");
+      resolve(bodyArray[GENERAL_USE_COUNT_INDEX].split(":"));
+    });
+    request.send();
+  })
+}
+
+function getGeneralBaseDataFromApi() {
+  return new Promise((resolve) => {
+    const BASE_DATA_API_URL = "https://3594t.net/datalist/api/base";
+    const GENERAL_DATA_INDEX = 6;
+    const request = new XMLHttpRequest();
+    request.open("GET", BASE_DATA_API_URL);
+    request.addEventListener("load", (event) => {
+      if (event.target.status !== 200) {
+        alert("データ取得に失敗しました、ステータスコード：" + event.target.status);
+        return;
+      }
+      const bodyArray = event.target.responseText.split("\n");
+      resolve(bodyArray[GENERAL_DATA_INDEX].split(":"));
+    });
+    request.send();
+  })
+}
+
+function getGeneralUseCountByAvatarHash(generalIndexAndUseCountList, generalBaseDataList, avatarHash) {
+  const targetGeneralIndex = generalBaseDataList.findIndex((general) => {
+    return general.split(",")[2] == avatarHash;
+  });
+  const targetGeneralIndexAndUseCount = generalIndexAndUseCountList.find((generalIndexAndUseCount) => {
+    return generalIndexAndUseCount.split(",")[0] == targetGeneralIndex;
+  });
+  return parseInt(targetGeneralIndexAndUseCount.split(",")[1]);
 }
